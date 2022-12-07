7fdc413beaaab8664d40bb03893e3941a6744de6
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2022-12-06 23:25:41 -0400

    project

---
+++ src/screens/Players/index.tsx
@@ -0,0 +1,24 @@
+import { ButtonIcon } from '@components/ButtonIcon';
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { Input } from '@components/Input';
+
+import { Container, Form } from './styles';
+
+export function Players() {
+  return (
+    <Container>
+      <Header showBackButton />
+
+      <Highlight
+        title="Nome da turma"
+        subtitle="adicione a galera e separe os times"
+      />
+
+      <Form>
+        <Input placeholder="Nome da pessoa" autoCorrect={false} />
+        <ButtonIcon icon="add" />
+      </Form>
+    </Container>
+  );
+}
